0004484e6b70317c7a532087ae39528c58f61d6e
Youngwoo Pyo <serif@ajou.ac.kr>
2024-11-17 19:46:27 +0900

    add folder structure for sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Panel } from './components/Panel/Panel';
+import { Title } from './components/Text/Text';
 function App() {
   return (
     <div className="App" >
       <Panel>
-        Panel Test
+        <Title>Panel Test</Title>
         <div>asdf</div>
       </Panel>
     </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+export const Navbar = ({children}) => {
+  return (
+    <div>
+      asdf
+    </div>
+  )
+}
