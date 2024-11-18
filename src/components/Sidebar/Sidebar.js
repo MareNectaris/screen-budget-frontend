db9df9c46454951e68ebf8ac47cfb4e2170e3cc5
Youngwoo Pyo <serif@ajou.ac.kr>
2024-11-18 17:50:47 +0900

    implement Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,13 @@
 import "./Sidebar.css"
 
-export const Sidebar = ({children}) => {
+export const Sidebar = ({children, isOpen}) => {
   return (
+    <div className="sidebar-container">
+    {isOpen&&
     <div className="sidebar">
       {children}
+    </div>
+    }
     </div>
   )
 }
