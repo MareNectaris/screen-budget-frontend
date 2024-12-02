54a9cbd7b602bda0cfb189eeb8d79e1cc916cef2
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2024-12-02 18:37:08 +0900

    Change '세션 확인 중' to Loading'

--- src/pages/Loading/Loading.js
+++ src/pages/Loading/Loading.js
@@ -5,7 +5,7 @@ export const Loading = ({ text }) => {
   return (
     <div className="App flex-center">
       <Panel style={{ width: '512px' }}>
-        <div className="flex-col flex-center" style={{ gap: '16px' }}>
+        <div className="flex-col flex-center" style={{ gap: '12px' }}>
           <div className="spinner" />
           <TextboxLabel>{text ? text : '로딩 중'}</TextboxLabel>
         </div>
