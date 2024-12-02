44ab7f8655f2bd895851bca544687fe4727a855d
Youngwoo Pyo <serif@ajou.ac.kr>
2024-12-02 11:30:13 +0900

    finish login layout

--- src/components/Textbox/Textbox.js
+++ src/components/Textbox/Textbox.js
@@ -1,10 +1,12 @@
 import './Textbox.css';
-export const Textbox = ({ value, onChange }) => {
+export const Textbox = ({ value, onChange, type, onKeyDown }) => {
   return (
     <input
       className="textbox"
       value={value}
       onChange={({ target }) => onChange(target.value)}
+      type={type}
+      onKeyDown={onKeyDown}
     />
   );
 };
