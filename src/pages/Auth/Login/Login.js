f2c30863bb8774acad4d1319095b45ef981f8b25
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2024-12-02 11:15:30 +0900

    implement Landing

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,23 +1,17 @@
 import { useNavigate } from 'react-router';
 import { Button } from '../../../components/Button/Button';
 import { Panel } from '../../../components/Panel/Panel';
-import { IntroTitle, Title } from '../../../components/Text/Text';
-export const Landing = () => {
+import { Title } from '../../../components/Text/Text';
+export const Login = () => {
   const navigate = useNavigate();
   return (
     <div className="App flex-center">
       <Panel style={{ width: '512px' }}>
         <div className="flex-col" style={{ gap: '12px' }}>
-          <div className="flex-center">
-            <div className="flex-col">
-              <div>허들 없는 가계부의 시작</div>
-              <IntroTitle>Screen Budget</IntroTitle>
-            </div>
-          </div>
           <div className="flex-col" style={{ gap: '8px' }}>
-            <Title>시작하기</Title>
-            <Button variant="contained" onClick={() => navigate('/login')}>
-              이메일로 계속하기
+            <Title>로그인</Title>
+            <Button variant="contained" onClick={() => alert('hi')}>
+              로그인
             </Button>
           </div>
         </div>
