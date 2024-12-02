4785a8d23cbce10d461b6bfa3f81306902de1eae
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2024-12-02 22:55:45 +0900

    Dashboard uses real values

--- src/pages/Book/individual/Dashboard/Dashboard.js
+++ src/pages/Book/individual/Dashboard/Dashboard.js
@@ -1,12 +1,91 @@
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { useMutation } from '@tanstack/react-query';
+import { floorAndFormatNumber } from '@toss/utils';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useOutletContext, useParams } from 'react-router';
+import { useNavigate, useOutletContext, useParams } from 'react-router';
+import { useRecoilState } from 'recoil';
 import { Line } from '../../../../components/Line/Line';
 import { Panel } from '../../../../components/Panel/Panel';
 import { Title } from '../../../../components/Text/Text';
+import { authState } from '../../../../store/Auth';
+import { LoadingNoBackground } from '../../../Loading/Loading';
 export const Dashboard = () => {
   const { bookUuid } = useParams();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const navigate = useNavigate();
+  const [auth, setAuth] = useRecoilState(authState);
+  const [monthly, setMonthly] = useState({ expense: 0, income: 0 });
+  const [today, setToday] = useState({ transactions: [], schedules: [] });
+  const [todayStats, setTodayStats] = useState({ expense: 0, income: 0 });
+  const [economyNews, setEconomyNews] = useState({ hankyung: [], mk: [] });
+
+  const calculateTodayExpensesAndIncome = () => {
+    let expenseSum = 0;
+    let incomeSum = 0;
+    today.transactions.forEach((transaction) => {
+      if (transaction.type === 'expense') {
+        expenseSum += transaction.amount;
+      } else {
+        incomeSum += transaction.amount;
+      }
+    });
+    setTodayStats({
+      expense: expenseSum,
+      income: incomeSum,
+    });
+  };
+  const config = {
+    headers: { Authorization: `${auth}` },
+  };
+
+  const dashboardPost = async (data) => {
+    const requests = [
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/transactions/${bookUuid}/monthly-summary`,
+        config
+      ),
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/transactions/${bookUuid}/todayTimeline`,
+        config
+      ),
+      axios.get(`${process.env.REACT_APP_SERVER_ADDRESS}/api/economy`),
+    ];
+
+    const responses = await Promise.all(requests);
+
+    return responses.map((res) => res.data);
+  };
+
+  const mutation = useMutation({
+    mutationFn: dashboardPost,
+    onSuccess: (dataArr) => {
+      const [_monthly, _today, _economyNews] = dataArr;
+      if (_monthly?.data) {
+        setMonthly(_monthly.data);
+      }
+      if (_today?.message) {
+        setToday(_today.message);
+      }
+      if (_economyNews?.data) {
+        setEconomyNews(_economyNews.data);
+      }
+    },
+    onError: (error) => {
+      alert(error);
+    },
+    onMutate: () => {
+      return <LoadingNoBackground />;
+    },
+  });
+
+  useEffect(() => {
+    calculateTodayExpensesAndIncome();
+  }, [today]);
+
+  useEffect(() => {
+    mutation.mutate({});
+  }, []);
+
   const { setMajorCategory, setMinorCategory } = useOutletContext();
   useEffect(() => {
     setMajorCategory('개인 가계부');
@@ -29,7 +108,9 @@ export const Dashboard = () => {
               <div className="flex-row flex-center pointer">
                 <div className="regular text-24px flex-1">이번 달 지출</div>
                 <div className="flex-row flex-center">
-                  <div className="bold text-36px">100,000원</div>
+                  <div className="bold text-36px">
+                    {floorAndFormatNumber(monthly.expense)}원
+                  </div>
                   <NavigateNextIcon />
                 </div>
               </div>
@@ -37,16 +118,22 @@ export const Dashboard = () => {
               <div className="flex-row flex-center pointer">
                 <div className="regular text-24px flex-1">오늘의 지출</div>
                 <div className="flex-row flex-center">
-                  <div className="bold text-32px">100,000원</div>
+                  <div className="bold text-32px">
+                    {floorAndFormatNumber(todayStats.expense)}원
+                  </div>
                   <NavigateNextIcon />
                 </div>
               </div>
               <Line />
-              <div className="flex-row flex-center pointer">
+              <div
+                className={`flex-row flex-center ${today.schedules.length > 0 && 'pointer'}`}
+              >
                 <div className="regular text-24px flex-1">오늘의 금융 일정</div>
                 <div className="flex-row flex-center">
-                  <div className="bold text-32px">2건</div>
-                  <NavigateNextIcon />
+                  <div className="bold text-32px">
+                    {today.schedules.length}건
+                  </div>
+                  {today.schedules.length > 0 && <NavigateNextIcon />}
                 </div>
               </div>
             </div>
