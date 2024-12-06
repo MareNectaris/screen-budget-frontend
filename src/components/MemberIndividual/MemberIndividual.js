da589c7591387a6bb32980736a42a49706e71cb6
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2024-12-06 18:55:54 +0900

    Add member add/delete, TODO compare with fetched memberId and exclude myself

---
+++ src/components/MemberIndividual/MemberIndividual.js
@@ -0,0 +1,15 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+export const MemberIndividual = ({ children, onDelete, _id }) => {
+  return (
+    <div className="flex-1 flex-row align-center gap-6px" key={_id}>
+      <AccountCircleIcon />
+      <div className="flex-1">{children}</div>
+      <DeleteIcon
+        sx={{ color: 'red' }}
+        className="pointer"
+        onClick={() => onDelete()}
+      />
+    </div>
+  );
+};
