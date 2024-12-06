68785b7e3b89242b644fa3cf316627d725725153
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2024-12-06 17:39:04 +0900

    add mui x piechart

--- src/pages/Book/individual/Stats/Stats.js
+++ src/pages/Book/individual/Stats/Stats.js
@@ -1,38 +1,36 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { Panel } from '../../../../components/Panel/Panel';
+import { Title } from '../../../../components/Text/Text';
 import { authState } from '../../../../store/Auth';
 export const Stats = () => {
   const navigate = useNavigate();
   const { setMajorCategory, setMinorCategory, books, setBooks } =
     useOutletContext();
   const [bookName, setBookName] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [perCategory, setPerCategory] = useState([]);
+  const [budgetFilteredPerCategory, setBudgetFilteredPerCategory] = useState(
+    []
+  );
+  const [spendingsFilteredPerCategory, setSpendingsFilteredPerCategory] =
+    useState([]);
   const [categories, setCategories] = useState([]);
   const [paymentMethods, setPaymentMethods] = useState([]);
-  const [selectedRadio, setSelectedRadio] = useState();
-  const [selectedRadioModify, setSelectedRadioModify] = useState();
-  const [newTransactionName, setNewTransactionName] = useState('');
-  const [modifyTransactionName, setModifyTransactionName] = useState('');
-  const [modifyCategoryId, setModifyCategoryId] = useState(null);
-  const [newCategoryId, setNewCategoryId] = useState(null);
-  const [newPaymentMethodId, setNewPaymentMethodId] = useState(null);
-  const [modifyPaymentMethodId, setModifyPaymentMethodId] = useState(null);
-  const [newAmount, setNewAmount] = useState('');
-  const [modifyAmount, setModifyAmount] = useState('');
-  const [isScheduleModalOpen, setIsScheduleModalOpen] = useState(false);
-  const [selectedSchedule, setSelectedSchedule] = useState(null);
-  const handleScheduleOpen = (key) => {
-    setSelectedSchedule(key);
-    setIsScheduleModalOpen(true);
-  };
-  const handleRadioChange = (val) => {
-    setSelectedRadio(val);
-  };
-  const handleRadioModifyChange = (val) => {
-    setSelectedRadioModify(val);
-  };
+  const [spendings, setSpendings] = useState({
+    daily: {
+      dailyBudget: 0,
+      dailySpent: 0,
+    },
+    monthly: {
+      monthlyBudget: 0,
+      monthlySpent: 0,
+    },
+  });
+
   const { bookUuid } = useParams();
   const getKSTDate = (date) => {
     const kstOffset = 9 * 60 * 60 * 1000;
@@ -42,29 +40,136 @@ export const Stats = () => {
     const d = kstDate.getUTCDate();
     return [y, m, d];
   };
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedYear, setSelectedYear] = useState(getKSTDate(new Date())[0]);
-  const [selectedMonth, setSelectedMonth] = useState(getKSTDate(new Date())[1]);
-  const [transactionsOnSelectedMonth, setTransactionsOnSelectedMonth] =
-    useState([]);
-  const [transactionsOnSelectedDate, setTransactionsOnSelectedDate] = useState(
-    []
-  );
   const auth = useRecoilValue(authState);
   const config = {
     headers: { Authorization: `${auth}` },
   };
 
+  const perCategoryGet = async (record) => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_SERVER_ADDRESS}/api/budgets/${bookUuid}/categories`,
+      config
+    );
+    return response.data;
+  };
+  const perCategoryGetMutation = useMutation({
+    mutationFn: perCategoryGet,
+    onSuccess: (data) => {
+      setPerCategory(data);
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
+  const paymentGet = async (data) => {
+    const requests = [
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/categories/${bookUuid}`,
+        config
+      ),
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/paymentMethods/${bookUuid}`,
+        config
+      ),
+    ];
+
+    const responses = await Promise.all(requests);
+
+    return responses.map((res) => res.data);
+  };
+
+  const paymentMutation = useMutation({
+    mutationFn: paymentGet,
+    onSuccess: (dataArr) => {
+      const [_categories, _paymentMethods] = dataArr;
+      if (_categories?.data) {
+        setCategories(_categories.data);
+      }
+      if (_paymentMethods?.data) {
+        setPaymentMethods(_paymentMethods.data);
+      }
+    },
+    onError: (error) => {
+      alert(error);
+    },
+    onMutate: () => {},
+  });
+
+  const spendingsGet = async (data) => {
+    const requests = [
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/budgets/${bookUuid}/daily`,
+        config
+      ),
+      axios.get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/budgets/${bookUuid}/monthly`,
+        config
+      ),
+    ];
+
+    const responses = await Promise.all(requests);
+
+    return responses.map((res) => res.data);
+  };
+
+  const spendingsGetMutation = useMutation({
+    mutationFn: spendingsGet,
+    onSuccess: (dataArr) => {
+      const [_daily, _monthly] = dataArr;
+      if (_daily && _monthly) {
+        setSpendings({
+          daily: _daily,
+          monthly: _monthly,
+        });
+      }
+    },
+    onError: (error) => {
+      alert(error);
+    },
+    onMutate: () => {},
+  });
+
+  useEffect(() => {
+    setBudgetFilteredPerCategory(
+      perCategory.map((elem, idx) => {
+        return {
+          id: idx,
+          label: elem.name,
+          color: elem.color,
+          value: elem.monthlyBudget,
+        };
+      })
+    );
+    setSpendingsFilteredPerCategory(
+      perCategory.map((elem, idx) => {
+        return {
+          id: idx,
+          label: elem.name,
+          color: elem.color,
+          value: elem.monthlySpent,
+        };
+      })
+    );
+  }, [perCategory]);
+  useEffect(() => {
+    console.log(budgetFilteredPerCategory);
+  }, [budgetFilteredPerCategory]);
   useEffect(() => {
     setMajorCategory(bookName);
   }, [bookName]);
   useEffect(() => {
     setBookName(books.find((elem) => elem._id == bookUuid)?.name);
     setMinorCategory('통계');
+    paymentMutation.mutate();
+    perCategoryGetMutation.mutate();
+    spendingsGetMutation.mutate();
   }, []);
   useEffect(() => {
     setBookName(books.find((elem) => elem._id == bookUuid)?.name);
   }, [books]);
+  useEffect(() => {
+    console.log(spendings);
+  }, [spendings]);
   return (
     <div
       style={{
@@ -82,11 +187,37 @@ export const Stats = () => {
           gridGap: '16px',
         }}
       >
-        <Panel></Panel>
-        <Panel></Panel>
+        <Panel>
+          <div className="flex-col">
+            <Title>지출 목표 대비 소비</Title>
+          </div>
+        </Panel>
+        <Panel>
+          <div className="flex-col">
+            <Title>예산 분배 현황</Title>
+            <div style={{ display: 'flex', flex: 1, justifyContent: 'center' }}>
+              <PieChart
+                series={[{ data: [...budgetFilteredPerCategory] }]}
+                height={400}
+                width={400}
+              />
+            </div>
+          </div>
+        </Panel>
       </div>
       <div style={{ maxHeight: '100%' }}>
-        <Panel style={{ height: '100%' }}></Panel>
+        <Panel style={{ height: '100%' }}>
+          <div className="flex-col">
+            <Title>카테고리별 분석</Title>
+            <div style={{ display: 'flex', flex: 1, justifyContent: 'center' }}>
+              <PieChart
+                series={[{ data: [...spendingsFilteredPerCategory] }]}
+                height={500}
+                width={800}
+              />
+            </div>
+          </div>
+        </Panel>
       </div>
     </div>
   );
